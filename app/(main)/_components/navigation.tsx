0c9af666247db29274d4352f6458c93f52dea21b
Alex <alexreeves17@gmail.com>
2024-06-19 08:02:38 -0500

    Added functionality to restore from archive and delete permanently

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useState, useRef, ElementRef, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -11,6 +11,8 @@ import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Trashbox } from "./trash-box";
 
 
 
@@ -170,6 +172,16 @@ export const Navigation = () => {
                         icon={Plus}
                         label="Add Doc"
                     />
+                    <Popover>
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                            className="p-0 w-72"
+                            side={isMobile ? "bottom" : "right"}>
+                                <Trashbox />
+                        </PopoverContent>
+                    </Popover>
                 </div>
 
                 {/*Create sidebar boundary for resizing*/}
